5281948db2540cde202d83d86ce514ef7e1c59d4
samu1oto <samuioto@outlook.com>
2025-11-01 13:39:57 +0800

    wrong delete

---
+++ lib/src/ui/shortcut/shortcuts.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+Map<ShortcutActivator, Intent> get defaultTerminalShortcuts {
+  switch (defaultTargetPlatform) {
+    case TargetPlatform.android:
+    case TargetPlatform.fuchsia:
+    case TargetPlatform.linux:
+    case TargetPlatform.windows:
+      return _defaultShortcuts;
+    case TargetPlatform.iOS:
+    case TargetPlatform.macOS:
+      return _defaultAppleShortcuts;
+    case TargetPlatform.ohos:
+      return _defaultShortcuts;
+    default:
+      return _defaultShortcuts;
+  }
+}
+
+final _defaultShortcuts = {
+  SingleActivator(LogicalKeyboardKey.keyC, control: true, shift: true):
+      CopySelectionTextIntent.copy,
+  SingleActivator(LogicalKeyboardKey.keyV, control: true, shift: true):
+      const PasteTextIntent(SelectionChangedCause.keyboard),
+  SingleActivator(LogicalKeyboardKey.keyA, control: true, shift: true):
+      const SelectAllTextIntent(SelectionChangedCause.keyboard),
+};
+
+final _defaultAppleShortcuts = {
+  SingleActivator(LogicalKeyboardKey.keyC, meta: true):
+      CopySelectionTextIntent.copy,
+  SingleActivator(LogicalKeyboardKey.keyV, meta: true):
+      const PasteTextIntent(SelectionChangedCause.keyboard),
+  SingleActivator(LogicalKeyboardKey.keyA, meta: true):
+      const SelectAllTextIntent(SelectionChangedCause.keyboard),
+};
